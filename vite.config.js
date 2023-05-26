9cd1b0dc2e1fca6c8af436ee3bd52f218d813403
solstice23 <solstice233@gmail.com>
2023-05-26 20:54:23 +0800

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		ViteImageOptimizer({
+  			test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
+			png: {
+				quality: 80,
+			},
+			jpg: {
+				quality: 80,
+			},
+		}),
+	],
 })
